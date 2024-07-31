1436ca8d9a154c262e7d3fc4b94266a02157a93c
Vishal Nagle <nagle.vishal@yahoo.in>
2024-07-31 15:06:12 +0530

    mocking axios code addded

--- tests/swappiGetter.test.js
+++ tests/swappiGetter.test.js
@@ -1,9 +1,18 @@
-import swapiGetter from "../functions/swappiGetter"
+import swapiGetter from "../functions/swappiGetter";
+import mockAxios from "axios";
+
+
+jest.mock("axios")
+
+mockAxios.get.mockResolvedValue({ data: { name: "Luke Skywalker" } })
 
 
 describe("swappiGetter GET api test", () => {
+    afterEach(jest.clearAllMocks)
+
     test("should return a name", async () => {
         const result = await swapiGetter(1);
         expect(result).toBe("Luke Skywalker")
+        expect(mockAxios.get).toHaveBeenCalledTimes(1);
     })
 })
